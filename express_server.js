4d3a553ce983c6defe9be1adb858ff9e2bddccf8
Irha Ali <irhaali_97@hotmail.ca>
2023-01-04 16:51:01 -0500

    Add some html on the server side

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+// Set up the express web server
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -7,14 +8,21 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// Try out some text on the main page
 app.get("/", (req, res) => {
   res.send("Hello!");
+});
+
+// Get json of the url database
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+// Try out some html in the server
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});u
