355fdc2223fcb4f2d363eed920d302799cad07f4
Irha Ali <irhaali_97@hotmail.ca>
2023-01-13 02:12:09 -0500

    Get a rough setup of the signin/out feat using cookies

--- express_server.js
+++ express_server.js
@@ -1,131 +1,117 @@
 // Import the backend functions
-const { generateRandomString } = require('./backend/genRandString');
+const { generateRandomString } = require('./backend/helperFunctions');
 
 // Set up the express web server
 const express = require("express");
-// const cookieParser = require('cookie-parser');
 const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
-let loginStat = false;
-
-// Init the lib
-// app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
 
 // temporary database for users
-const USERS = {
+const users = {
   'user1': 'password1',
   'user2': 'password2'
 };
 
 // Starting database
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  'user1': { "b2xVn2": "http://www.lighthouselabs.ca" },
+  'user2': { "9sm5xK": "http://www.google.com" }
 };
 
 // Main page
 app.get("/", (req, res) => {
-  const templateVars = { loginStat };
-  res.render("pages/main", templateVars);
+  const user = req.cookies.user;
+  res.render("pages/main", { user });
 });
 
 // Page for user's URLs
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, loginStat };
+  const user = req.cookies.user;
+  const templateVars = { urls: (user) ? urlDatabase[user] : {}, user };
   res.render("pages/urls_index", templateVars);
-});
-
-// Get json of the url database
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-// Try out a page with html embedded in it
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
-// Try out a page with html outsourced to .ejs file
-app.get("/hellothere", (req, res) => {
-  const templateVars = { greeting: "Hello World!", loginStat };
-  res.render("pages/hello_world", templateVars);
 });
 
 // Page for adding a new url
 app.get("/urls/new", (req, res) => {
-  const templateVars = { urls: urlDatabase, loginStat };
+  const user = req.cookies.user;
+  const templateVars = { urls: urlDatabase[user], user };
   res.render("pages/urls_new", templateVars);
 });
 
 // Page for a specified url
 app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
-  if (urlDatabase[id] === undefined) {
-    res.render("pages/404", loginStat);
+  const user = req.cookies.user;
+  if ((!user) || (urlDatabase[user][id] === undefined)) {
+    res.render("pages/404", user);
   }
-  const templateVars = { id, longURL: urlDatabase[id], loginStat };
+  const templateVars = { id, longURL: urlDatabase[user][id], user };
   res.render("pages/urls_show", templateVars);
 });
 
 // Redirect to the longURL that the shortURL is hyperlinked to
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id];
+  const user = req.cookies.user;
+  const longURL = urlDatabase[user][req.params.id];
   res.redirect(longURL);
 });
 
 // Redirect to login/signup page
 app.get("/login", (req, res) => {
-  if (loginStat) {
-    loginStat = false;
-    const templateVars = { loginStat };
-    res.render('pages/main', templateVars);
+  let user = req.cookies.user;
+  if (user) {
+    res.clearCookie('email');
+    const user = req.cookies.user;
+    res.render('pages/main', { user });
   } else {
-    const templateVars = { loginStat };
-    res.render("pages/login", templateVars);
+    res.render("pages/login", { user });
   }
 });
 
 // After login form is filled
 app.post('/login', (req, res) => {
   const email = req.body['email'];
-  const password = USERS[email];
+  const password = users[email];
   if (password === req.body['password']) {
-    loginStat = true;
-    const templateVars = { urls: urlDatabase, loginStat };
+    res.cookie('user', email);
+    const user = req.cookies.user;
+    const templateVars = { urls: urlDatabase[user], user };
     res.render("pages/urls_index", templateVars);
   } else {
-    const templateVars = { loginStat };
-    res.render("pages/login", templateVars);
+    res.render("pages/login", { undefined });
   }
 });
 
 // After signup form is filled
 app.post('/signup', (req, res) => {
   const email = req.body['email'];
-  if (USERS[email] === undefined) {
-    loginStat = true;
-    USERS[email] = req.body['password'];
-    const templateVars = { loginStat };
-    res.render("pages/urls_new", templateVars);
+  if (users[email] === undefined) {
+    const user = req.cookies.user;
+    users[email] = req.body['password'];
+    res.cookie('user', email);
+    res.render("pages/urls_new", { user });
   } else {
-    const templateVars = { loginStat };
-    res.render("pages/login", templateVars);
+    res.render("pages/login", { undefined });
   }
 });
 
 // Redirect to /urls after a url is destroyed
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
-  const templateVars = { urls: urlDatabase, loginStat };
+  delete urlDatabase[user][req.params.id];
+  const user = req.cookies.user;
+  const templateVars = { urls: urlDatabase[user], user };
   res.render("pages/urls_index", templateVars);
 });
 
 // Redirect to /urls after an existing url is edited
 app.post("/urls/:id/edit", (req, res) => {
-  urlDatabase[req.params.id] = req.body['longURL'];
-  const templateVars = { urls: urlDatabase, loginStat };
+  urlDatabase[user][req.params.id] = req.body['longURL'];
+  const user = req.cookies.user;
+  const templateVars = { urls: urlDatabase[user], user };
   res.render("pages/urls_index", templateVars);
 });
 
@@ -135,15 +121,16 @@ app.post("/urls/add", (req, res) => {
   if (shortURL === "" || shortURL === null || shortURL === undefined) {
     shortURL = generateRandomString();
   }
-  urlDatabase[shortURL] = req.body['longURL'];
-  const templateVars = { urls: urlDatabase, loginStat };
+  const user = req.cookies.user;
+  urlDatabase[user][shortURL] = req.body['longURL'];
+  const templateVars = { urls: urlDatabase[user], user };
   res.render("pages/urls_index", templateVars);
 });
 
 // Custom 404 for all non-existing pages
 app.get('*', function(req, res) {
-  const templateVars = { loginStat };
-  res.render("pages/404", templateVars);
+  const user = req.cookies.user;
+  res.render("pages/404", { user });
 });
 
 app.listen(PORT, () => {
