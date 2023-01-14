bfc11ffb122323255d6d6c0c0868dea83c246d94
Irha Ali <irhaali_97@hotmail.ca>
2023-01-13 23:00:21 -0500

    Add express-validator for some of the forms

--- express_server.js
+++ express_server.js
@@ -11,12 +11,15 @@ app.set('trust proxy', 1);
 
 app.use(cookieSession({
   name: 'session',
-  keys: ['key1', 'key2'],
+  keys: ['key1'],
 }));
+
 const bcrypt = require("bcryptjs");
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 const PORT = 8080; // default port 8080
+
+const { check, validationResult } = require('express-validator');
 
 // Starting database for users
 const users = {
@@ -37,7 +40,7 @@ app.get("/", (req, res) => {
 
 // Redirect to login/signup page
 app.get("/login", (req, res) => {
-  res.render("pages/login", { user: undefined });
+  res.render("pages/login", { alert: {}, user: undefined });
 });
 
 // Page for user's URLs
@@ -59,9 +62,9 @@ app.get("/urls/:id", (req, res) => {
   const id = req.params.id;
   const user = req.session.user;
   if (urlDatabase[user][id] === undefined) {
-    res.render("pages/404", { user });
+    res.redirect("pages/404");
   }
-  const templateVars = { id, longURL: urlDatabase[user][id], user };
+  const templateVars = { id, url: urlDatabase[user], user };
   res.render("pages/urls_show", templateVars);
 });
 
@@ -70,30 +73,59 @@ app.get("/u/:id", (req, res) => {
   res.redirect(urlDatabase[req.session.user][req.params.id]);
 });
 
-// After login form is filled
-app.post('/login', (req, res) => {
+// After login button is pressed
+app.post('/login', [
+  check('email', 'Email field is required')
+    .exists()
+    .isLength({ min: 1 }),
+  check('password', 'Password field is required')
+    .exists()
+    .isLength({ min: 1 }),
+], (req, res) => {
+  // Empty fields alert
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const alert = { login: errors.array() };
+    const templateVars = { alert, user: undefined };
+    res.render('pages/login', templateVars);
+  }
   const email = req.body['email'];
   const hashedPassword = (users[email]) ? users[email] : '';
   if (bcrypt.compareSync(req.body['password'], hashedPassword)) {
     req.session.user = email;
-    const templateVars = { urls: urlDatabase[email], user: email };
-    res.render("pages/urls_index", templateVars);
+    res.redirect("/urls");
   } else {
-    res.render("pages/login", { user: undefined });
+    const alert = { login: [{ msg: "Incorrect Password/Account does not exists" }] };
+    const templateVars = { alert, user: undefined };
+    res.render("pages/login", templateVars);
   }
 });
 
 // After signup form is filled
-app.post('/signup', (req, res) => {
+app.post('/signup', [
+  check('email', 'Email field is required')
+    .exists()
+    .isLength({ min: 1 }),
+  check('password', 'Password does not match/A password field is empty')
+    .exists()
+    .isLength({ min: 1 }),
+  check('retype-password', 'Password does not match/A password field is empty')
+    .exists()
+    .equals()
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const alert = { signup: errors.array() };
+    res.render('pages/login', { alert });
+  }
   const email = req.body['email'];
   if (users[email] === undefined) {
     users[email] = bcrypt.hashSync(req.body['password'], 10);
     urlDatabase[email] = {};
     req.session.user = email;
-    const templateVars = { urls: urlDatabase[email], user: email };
-    res.render("pages/urls_new", templateVars);
+    res.redirect("/urls/new");
   } else {
-    res.render("pages/login", { user: undefined });
+    res.redirect("/login");
   }
 });
 
@@ -101,16 +133,14 @@ app.post('/signup', (req, res) => {
 app.post("/urls/:id/delete", (req, res) => {
   const user = req.session.user;
   delete urlDatabase[user][req.params.id];
-  const templateVars = { urls: urlDatabase[user], user };
-  res.render("pages/urls_index", templateVars);
+  res.redirect("/urls");
 });
 
 // Redirect to /urls after an existing url is edited
 app.post("/urls/:id/edit", (req, res) => {
   const user = req.session.user;
   urlDatabase[user][req.params.id] = req.body['longURL'];
-  const templateVars = { urls: urlDatabase[user], user };
-  res.render("pages/urls_index", templateVars);
+  res.redirect("/urls");
 });
 
 // Redirect to /urls after a url is added
@@ -121,13 +151,12 @@ app.post("/urls/add", (req, res) => {
   }
   const user = req.session.user;
   urlDatabase[user][shortURL] = req.body['longURL'];
-  const templateVars = { urls: urlDatabase[user], user };
-  res.render("pages/urls_index", templateVars);
+  res.redirect("/urls");
 });
 
 // logout
 app.post('/logout', (req, res) => {
-  res.clearCookie('user');
+  req.session = null;
   res.redirect('/login');
 });
 
